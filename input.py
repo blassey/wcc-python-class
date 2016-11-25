894344d822e132dc674435e2657382d8d3f28205
blassey <valerie@blassey.com>
2016-11-24 23:57:29 -0500

    Completed challenge set 2

---
+++ input.py
@@ -0,0 +1,17 @@
+#name = raw_input('What is your name? ')
+#print('Hi ' + name)
+
+#name = raw_input('What is your name?')
+#age = raw_input('How old are you?')
+#print(name + ' is ' + age + ' years old.')
+
+#age = raw_input('How old are you? ')
+#dog_years = int(age) * 7
+#print('You are ' + str(dog_years) + ' years old in dog years.')
+
+cost = raw_input('How much was your meal? ')
+tip = float(cost) * 0.20
+total_cost = float(cost) + float(tip)
+
+print('You should tip $' + str(tip))
+print('Your total cost would be $' + str(total_cost))
--- variables.py
+++ variables.py
@@ -3,3 +3,11 @@ y = x + 1
 
 print(x)
 print(y)
+
+age = 30
+next_age = age + 1
+birth_month = 'August'
+birth_stone = 'peridot'
+
+print('Ariel is ' + str(age) + ' years old now, but she\'ll turn ' + str(next_age) + ' in ' + birth_month + '.')
+print('Her birth stone is ' + birth_stone + '.')
